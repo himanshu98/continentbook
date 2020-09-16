d6940d969e4c8bd964d0bda25a294d5c586bffcd
himanshu.tomar <tomarhimanshu98@gmail.com>
2020-09-16 22:08:13 +0530

    Updates

---
+++ precache-manifest.32d3b3d0f569c0932eb57587619fdb33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c3388777d4fc0508f00592230f7608",
+    "url": "/continentbook/index.html"
+  },
+  {
+    "revision": "c96530edf576dca7c5c4",
+    "url": "/continentbook/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "68c92ef27a685c06ebc1",
+    "url": "/continentbook/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c96530edf576dca7c5c4",
+    "url": "/continentbook/static/js/2.47fa6a53.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/continentbook/static/js/2.47fa6a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c92ef27a685c06ebc1",
+    "url": "/continentbook/static/js/main.584e8da1.chunk.js"
+  },
+  {
+    "revision": "2922b7e83ef69d9d815a",
+    "url": "/continentbook/static/js/runtime-main.fd030d13.js"
+  }
+]);
